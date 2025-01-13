80f0d432038ccfc401fc591ec8e4cdac60543b54
Freek <freek_mencke@outlook.com>
2025-01-13 20:57:00 +0100

    feat(about): added about page and re-enabled clientHydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideClientHydration, withEventReplay, withIncrementalHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
@@ -8,5 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })),
     provideExperimentalZonelessChangeDetection(),
+    provideClientHydration(withIncrementalHydration(), withEventReplay()),
   ],
 };
--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TargetBlankDirective } from '../../common/target-blank/target-blank.directive';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TargetBlankDirective],
 })
 export class AboutComponent {}
